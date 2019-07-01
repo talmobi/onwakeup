2d02726931eaa3fb9116ebee59ddf694d96a3fd3
Christian Talmo <talmo.christian@gmail.com>
2019-07-02 02:58:00 +0300

    linted

--- onwakeup.js
+++ onwakeup.js
@@ -7,46 +7,46 @@ var _lastTime = Date.now()
 var _listeners = []
 var _interval
 
-function addEventListener (callback) {
-  if (typeof callback !== 'function') {
-    throw new Error('Error! a callback function must be supplied.')
+function addEventListener ( callback ) {
+  if ( typeof callback !== 'function' ) {
+    throw new Error( 'Error! a callback function must be supplied.' )
   } else {
-    if (!_interval) {
-      _interval = setInterval(function () {
+    if ( !_interval ) {
+      _interval = setInterval( function () {
         var now = Date.now()
-        var limit = (_lastTime + INTERVAL + PADDING)
-        if (now > limit) {
-          var delta = (now - limit)
+        var limit = ( _lastTime + INTERVAL + PADDING )
+        if ( now > limit ) {
+          var delta = ( now - limit )
 
-          _listeners.forEach(function (callback) {
-            callback(delta)
-          })
-          console.log('delta:' + (now - limit))
+          _listeners.forEach( function ( callback ) {
+            callback( delta )
+          } )
+          console.log( 'delta:' + ( now - limit ) )
         } else {
-          console.log('tick ' + (new Date()))
+          console.log( 'tick ' + ( new Date() ) )
         }
         _lastTime = now
-      }, INTERVAL)
+      }, INTERVAL )
     }
 
-    _listeners.push(callback)
+    _listeners.push( callback )
 
     var off = function () {
-      var i = _listeners.indexOf(callback)
-      if (_listeners.length === 0) {
-        clearInterval(_interval)
+      var i = _listeners.indexOf( callback )
+      if ( _listeners.length === 0 ) {
+        clearInterval( _interval )
         _interval = undefined
       }
-      return _listeners.splice(i, 1)
+      return _listeners.splice( i, 1 )
     }
 
     return off
   }
 }
 
-function init (callback) {
-  if (typeof callback === 'function') {
-    return addEventListener(callback)
+function init ( callback ) {
+  if ( typeof callback === 'function' ) {
+    return addEventListener( callback )
   }
 }
 
